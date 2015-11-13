fa0a78e4c07706fcfb9c4a6cec078792c06bdecf
Wong Hoi Sing Edison <hswong3i@gmail.com>
2015-11-13 15:19:29 +0800

    Better handy scripts implementation.

---
+++ scripts/bootstrap-hosts.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -o xtrace
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR/../
+
+# Copy hosts files from template.
+echo -e "\n# Updated on `date`" >> hosts
+cat >> hosts <<-EOF
+[bitbucket]
+localhost   ansible_connection=local
+
+[postgresql]
+localhost   ansible_connection=local
+
+#[mysql]
+#localhost   ansible_connection=local
+
+#[apache2_all]
+#localhost   ansible_connection=local
+EOF
--- scripts/run-aio-build.sh
+++ scripts/run-aio-build.sh
@@ -3,16 +3,17 @@
 set -o xtrace
 
 # Install basic packages for this script.
-apt-get -y install git software-properties-common
+apt-get -y install software-properties-common
 apt-add-repository -y ppa:ansible/ansible
 apt-get update
-apt-get -y install ansible
+apt-get -y install ansible git
 
 # GIT clone our playbook to CWD.
-git clone https://github.com/pantarei/ansible-playbook-bitbucket.git ansible-playbook-bitbucket
-cd ansible-playbook-bitbucket
-git checkout master
+git clone https://github.com/pantarei/ansible-playbook-bitbucket.git -b master ansible-playbook-bitbucket
 
 # Bootstrap Ansible then run all playbooks.
-scripts/bootstrap-ansible.sh
+cd ansible-playbook-bitbucket
+scripts/bootstrap-roles.sh
+scripts/bootstrap-vars.sh
+scripts/bootstrap-hosts.sh
 scripts/run-playbooks.sh
