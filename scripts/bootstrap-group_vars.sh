24218de6004903f550c457b7478014e389c04072
Wong Hoi Sing Edison <hswong3i@gmail.com>
2016-04-18 21:20:33 +0800

    sync new uid/gid default settings.

--- scripts/bootstrap-group_vars.sh
+++ scripts/bootstrap-group_vars.sh
@@ -17,6 +17,7 @@ echo -en '\n' >> $TMP_VARS
 perl -i -p -e "s/^#(apache2_http_port):.*/\1: 80/g" $TMP_VARS
 perl -i -p -e "s/^#(apache2_https_port):.*/\1: 443/g" $TMP_VARS
 perl -i -p -e "s/^#(apache2_vhosts_document_root):.*/\1: ~/g" $TMP_VARS
+perl -i -p -e "s/^#(apache2_vhosts_gid):.*/\1: ~/g" $TMP_VARS
 perl -i -p -e "s/^#(apache2_vhosts_hash_salt):.*/\1: \"$SALT\"/g" $TMP_VARS
 perl -i -p -e "s/^#(apache2_vhosts_http_port):.*/\1: \"{{ apache2_http_port }}\"/g" $TMP_VARS
 perl -i -p -e "s/^#(apache2_vhosts_https_port):.*/\1: \"{{ apache2_https_port }}\"/g" $TMP_VARS
@@ -29,12 +30,15 @@ perl -i -p -e "s/^#(apache2_vhosts_proxy_request):.*/\1: 'On'/g" $TMP_VARS
 perl -i -p -e "s/^#(apache2_vhosts_proxy_via):.*/\1: 'On'/g" $TMP_VARS
 perl -i -p -e "s/^#(apache2_vhosts_server_alias):.*/\1: []/g" $TMP_VARS
 perl -i -p -e "s/^#(apache2_vhosts_server_name):.*/\1: bitbucket.example.com/g" $TMP_VARS
+perl -i -p -e "s/^#(apache2_vhosts_uid):.*/\1: ~/g" $TMP_VARS
 perl -i -p -e "s/^#(apache2_vhosts_user):.*/\1: bitbucket/g" $TMP_VARS
 perl -i -p -e "s/^#(apt_cache_valid_time):.*/\1: 3600/g" $TMP_VARS
 perl -i -p -e "s/^#(apt_upgrade):.*/\1: full/g" $TMP_VARS
+perl -i -p -e "s/^#(bitbucket_gid):.*/\1: \"{{ apache2_vhosts_gid }}\"/g" $TMP_VARS
 perl -i -p -e "s/^#(bitbucket_hash_salt):.*/\1: \"{{ apache2_vhosts_hash_salt }}\"/g" $TMP_VARS
 perl -i -p -e "s/^#(bitbucket_pass):.*/\1: \"{{ apache2_vhosts_pass }}\"/g" $TMP_VARS
 perl -i -p -e "s/^#(bitbucket_proxy_name):.*/\1: \"{{ apache2_vhosts_server_name }}\"/g" $TMP_VARS
+perl -i -p -e "s/^#(bitbucket_uid):.*/\1: \"{{ apache2_vhosts_uid }}\"/g" $TMP_VARS
 perl -i -p -e "s/^#(bitbucket_user):.*/\1: \"{{ apache2_vhosts_user }}\"/g" $TMP_VARS
 perl -i -p -e "s/^#(mysql_connector_java_dest):.*/\1: \/usr\/share\/bitbucket\/lib/g" $TMP_VARS
 perl -i -p -e "s/^#(mysql_vhosts_collation):.*/\1: utf8_bin/g" $TMP_VARS
